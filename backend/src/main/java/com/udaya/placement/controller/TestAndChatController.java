aac391bfb2c95286bbe23f2d82ec332867a53bbe
Udaya Kumar K J <udaykjgowda@gmail.com>
2026-09-19 20:38:30 +0530

    cgpa

--- backend/src/main/java/com/udaya/placement/controller/TestAndChatController.java
+++ backend/src/main/java/com/udaya/placement/controller/TestAndChatController.java
@@ -17,8 +17,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
- js
- 
+
 /**
  * Mock Interview Chatbot Controller.
  */
