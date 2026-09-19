0c6e4051d2e895933433cca10edd6c06698fef2a
Udaya Kumar K J <udaykjgowda@gmail.com>
2026-09-19 20:40:52 +0530

    ai make the interview

--- backend/src/main/java/com/udaya/placement/controller/TestAndChatController.java
+++ backend/src/main/java/com/udaya/placement/controller/TestAndChatController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 /**
- * Mock Interview Chatbot Controller. make 
+ * Mock Interview Chatbot Controller. mak
  */
 
 @RestController
